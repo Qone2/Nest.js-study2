d677914a3164aa85a7d862f1f95dc9ed92156381
qone <qone@dayside.co.kr>
2024-02-16 16:04:41 +0900

    섹션6-1까지 학습

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
@@ -9,30 +9,30 @@ export class BoardsController {
   constructor(private boardService: BoardsService) {
   }
 
-  @Get()
-  getAllBoard(): Board[] {
-    return this.boardService.getAllBoards();
-  }
-
-  @Post()
-  @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    return this.boardService.createBoard(createBoardDto);
-  }
-
-  @Get('/:id')
-  getBoardById(@Param('id') id: string) {
-    return this.boardService.getBoardById(id);
-  }
-
-  @Delete('/:id')
-  deleteBoard(@Param('id') id: string): void{
-    this.boardService.deleteBoard(id);
-  }
-
-  @Patch('/:id/status')
-  updateBoardStatus(@Param('id') id: string,
-                    @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board {
-    return this.boardService.updateBoardStatus(id, status);
-  }
+  // @Get()
+  // getAllBoard(): Board[] {
+  //   return this.boardService.getAllBoards();
+  // }
+  //
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  //   return this.boardService.createBoard(createBoardDto);
+  // }
+  //
+  // @Get('/:id')
+  // getBoardById(@Param('id') id: string) {
+  //   return this.boardService.getBoardById(id);
+  // }
+  //
+  // @Delete('/:id')
+  // deleteBoard(@Param('id') id: string): void{
+  //   this.boardService.deleteBoard(id);
+  // }
+  //
+  // @Patch('/:id/status')
+  // updateBoardStatus(@Param('id') id: string,
+  //                   @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board {
+  //   return this.boardService.updateBoardStatus(id, status);
+  // }
 }
