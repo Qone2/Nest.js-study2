4fb25b0eaf62caa1ad8f768a93b8e91850a04e22
qone <qone@dayside.co.kr>
2024-02-20 18:18:13 +0900

    섹션7 토큰생성 까지

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,20 @@ import { AuthService } from './auth.service';
 import { UserRepository } from "./user.repository";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./user.entity";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'secret1234',
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+    PassportModule.register({defaultStrategy: 'jwt'}),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository]
 })
